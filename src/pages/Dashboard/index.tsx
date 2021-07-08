e929467a7b00302de1c412ba37ffe08733692453
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-08 17:35:33 -0300

    Listando issues da API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
