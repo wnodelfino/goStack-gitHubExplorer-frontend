330564d49140c366cb42e19f6cb9a0d4cdb1bf5a
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-08 11:15:40 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/github-logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore reposotórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="test">
+        <img
+          src="https://avatars.githubusercontent.com/u/84684478?v=4"
+          alt="Wellynton Delfino"
+        />
+        <div>
+          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
+          <p>Primeira api com React!!!</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+      <a href="test">
+        <img
+          src="https://avatars.githubusercontent.com/u/84684478?v=4"
+          alt="Wellynton Delfino"
+        />
+        <div>
+          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
+          <p>Primeira api com React!!!</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+      <a href="test">
+        <img
+          src="https://avatars.githubusercontent.com/u/84684478?v=4"
+          alt="Wellynton Delfino"
+        />
+        <div>
+          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
+          <p>Primeira api com React!!!</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
