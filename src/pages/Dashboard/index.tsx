d0c4a9cf25f7fd4a09e99093fc29edeecab4cee1
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-08 13:13:17 -0300

    Conectando a api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,59 +1,70 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/github-logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    <Repositories>
-      <a href="test">
-        <img
-          src="https://avatars.githubusercontent.com/u/84684478?v=4"
-          alt="Wellynton Delfino"
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
-          <p>Primeira api com React!!!</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img
-          src="https://avatars.githubusercontent.com/u/84684478?v=4"
-          alt="Wellynton Delfino"
-        />
-        <div>
-          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
-          <p>Primeira api com React!!!</p>
-        </div>
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img
-          src="https://avatars.githubusercontent.com/u/84684478?v=4"
-          alt="Wellynton Delfino"
-        />
-        <div>
-          <strong>wnodelfino/goStack-gitHubExplorer-frontend</strong>
-          <p>Primeira api com React!!!</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
